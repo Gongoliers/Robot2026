4be6b4a01c2d444dcb71a9b3c16652748574a077
Hayden Heroux <haydenheroux@gmail.com>
2026-02-02 17:28:35 -0500

    Add `Objective`.

--- src/main/java/frc/robot/configuration/FieldRegion.java
+++ src/main/java/frc/robot/configuration/FieldRegion.java
@@ -2,27 +2,45 @@ package frc.robot.configuration;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 import static frc.robot.configuration.AndyMarkFieldMeasurements.*;
+import static frc.robot.configuration.Objective.*;
 
 public enum FieldRegion {
-    BLUE_ALLIANCE_ZONE(ZERO, ZERO, WIDTH, BLUE_HUB_X),
-    NEUTRAL_ZONE(ZERO, BLUE_HUB_X, WIDTH, LENGTH.minus(BLUE_HUB_X)),
-    RED_ALLIANCE_ZONE(ZERO, LENGTH.minus(BLUE_HUB_X), WIDTH, LENGTH);
+    BLUE_OUTPOST_ZONE(ZERO, ZERO, BLUE_HUB_X, Y_MIDLINE, SCORING, PASSING),
+    BLUE_DEPOT_ZONE(ZERO, ZERO, BLUE_HUB_X, Y_MIDLINE, SCORING, PASSING),
+    NEUTRAL_ZONE(ZERO, BLUE_HUB_X, WIDTH, LENGTH.minus(BLUE_HUB_X), PASSING, PASSING),
+    RED_DEPOT_ZONE(LENGTH.minus(BLUE_HUB_X), ZERO, LENGTH, Y_MIDLINE, PASSING, SCORING),
+    RED_OUTPOST_ZONE(LENGTH.minus(BLUE_HUB_X), Y_MIDLINE, LENGTH, Y_MIDLINE, PASSING, SCORING);
 
     private final Translation2d lowerLeftCorner;
 
     private final Translation2d upperRightCorner;
 
-    FieldRegion(Translation2d lowerLeftCorner, Translation2d upperRightCorner) {
+    private final Function<DriverStation.Alliance, Objective> objective_;
+
+    FieldRegion(Translation2d lowerLeftCorner, Translation2d upperRightCorner, Function<DriverStation.Alliance, Objective> objective) {
         this.lowerLeftCorner = lowerLeftCorner;
         this.upperRightCorner = upperRightCorner;
+        this.objective_ = objective;
     }
 
-    FieldRegion(Distance x1, Distance y1, Distance x2, Distance y2) {
-        this(new Translation2d(x1, y1), new Translation2d(x2, y2));
+    FieldRegion(Distance x1, Distance y1, Distance x2, Distance y2, Objective blueObjective, Objective redObjective) {
+        this(new Translation2d(x1, y1), new Translation2d(x2, y2), alliance -> {
+            switch (alliance) {
+                case Blue -> {
+                    return blueObjective;
+                }
+                case Red -> {
+                    return redObjective;
+                }
+            }
+            return Objective.NONE;
+        });
     }
 
     public boolean contains(Translation2d position) {
@@ -31,6 +49,10 @@ public enum FieldRegion {
         boolean inX = lowerLeftCorner.getX() <= x && x <= upperRightCorner.getX();
         boolean inY = lowerLeftCorner.getY() <= y && y <= upperRightCorner.getY();
         return inX && inY;
+    }
+
+    public Objective objective(DriverStation.Alliance alliance) {
+        return objective_.apply(alliance);
     }
 
     public static FieldRegion[] containing(Translation2d position) {
---
+++ src/main/java/frc/robot/configuration/Objective.java
@@ -0,0 +1,9 @@
+package frc.robot.configuration;
+
+public enum Objective {
+    NONE,
+    SCORING,
+    PASSING,
+    CLIMB,
+    DECLIMB
+}
