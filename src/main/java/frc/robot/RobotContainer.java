0694699374b458fcf319f683b35c67b35ae5d520
Hayden Heroux <haydenheroux@gmail.com>
2026-02-10 20:18:28 -0500

    Set up new `Objective` constants.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -66,6 +66,8 @@ public class RobotContainer {
 
   private final List<SendableChooser<Action>> choosers;
 
+  private final SendableChooser<Boolean> isLeftSide;
+
   /**
    * Gets robot container instance
    * 
@@ -106,6 +108,11 @@ public class RobotContainer {
       return chooser;
     }).toList();
     SmartDashboard.putString("Action", "");
+
+    isLeftSide = new SendableChooser<>();
+    isLeftSide.setDefaultOption("Left", true);
+    isLeftSide.addOption("Right", false);
+    SmartDashboard.putData("Is Left Side?", isLeftSide);
 
     multithreader.start();
 
@@ -148,6 +155,10 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     Action[] actions = choosers.stream().map(SendableChooser::getSelected).toArray(Action[]::new);
-    return ObjectiveActionMachine.createCommand(actions, drive::driveTo, this::logAction);
+    if (isLeftSide.getSelected()) {
+      return ObjectiveActionMachine.createLeftSideCommand(actions, drive::driveTo, this::logAction);
+    } else {
+      return ObjectiveActionMachine.createRightSideCommand(actions, drive::driveTo, this::logAction);
+    }
   }
 }
