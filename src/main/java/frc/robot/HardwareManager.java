4416206408779d79b8f8f20ecb499459c4489afb
Hayden Heroux <haydenheroux@gmail.com>
2026-02-22 22:08:16 -0500

    Documentation for `HardwareManager`.

--- src/main/java/frc/robot/HardwareManager.java
+++ src/main/java/frc/robot/HardwareManager.java
@@ -3,8 +3,18 @@ package frc.robot;
 import java.util.EnumSet;
 import java.util.Set;
 
+/**
+ * Manages what hardware is enabled.
+ * <p>
+ * Convention for "enabled" is that the hardware devices can be created without error.
+ * A natural use that arises from this convention is that device creation should be guarded by checking if the hardware is enabled.
+ */
 public class HardwareManager {
 
+    /**
+     * The sets of hardware that are being managed.
+     * There may be a one-to-one mapping from subsystem to hardware, or a hardware can encapsulate multiple subsystems.
+     */
     public enum Hardware {
         SWERVE,
         SHOOTER,
@@ -13,29 +23,57 @@ public class HardwareManager {
         CLIMB
     }
 
-    public static final Set<Hardware> ENABLED_HARDWARE = EnumSet.allOf(Hardware.class);
+    /**
+     * The set of hardware that is enabled. By default, all hardware is enabled, unless explicitly disabled.
+     */
+    private static final Set<Hardware> ENABLED_HARDWARE = EnumSet.allOf(Hardware.class);
 
+    /**
+     * Disables all hardware.
+     */
     public static void disableAll() {
         ENABLED_HARDWARE.clear();
     }
 
+    /**
+     * Disables these hardware.
+     *
+     * @param hardware The hardware to disable.
+     */
     public static void disable(Hardware... hardware) {
         for (Hardware h : hardware) {
             ENABLED_HARDWARE.remove(h);
         }
     }
 
+    /**
+     * Enables these hardware.
+     *
+     * @param hardware The hardware to enable.
+     */
     public static void enable(Hardware... hardware) {
+        // Equivalent to Collections.addAll(ENABLED_HARDWARE, hardware);
         for (Hardware h : hardware) {
             ENABLED_HARDWARE.add(h);
         }
     }
 
+    /**
+     * Guarantees that these hardware sets are enabled.
+     *
+     * @param hardware The hardware to enable.
+     */
     public static void enableOnly(Hardware... hardware) {
         disableAll();
         enable(hardware);
     }
 
+    /**
+     * Tests if this hardware is enabled.
+     *
+     * @param hardware The hardware to test.
+     * @return True if this hardware is enabled, false otherwise.
+     */
     public static boolean isEnabled(Hardware hardware) {
         return Robot.isReal() && ENABLED_HARDWARE.contains(hardware);
     }
