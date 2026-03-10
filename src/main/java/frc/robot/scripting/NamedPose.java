eb5ff209a0f75b4403adc1dbb005a88549ea32e4
Hayden Heroux <haydenheroux@gmail.com>
2026-03-10 18:21:38 -0400

    Update named poses

--- src/main/java/frc/robot/scripting/NamedPose.java
+++ src/main/java/frc/robot/scripting/NamedPose.java
@@ -9,16 +9,16 @@ import static edu.wpi.first.units.Units.*;
 
 public enum NamedPose {
     // TODO Use actual field measurements
-    NEUTRAL_LEFT(Meters.of(8), Meters.of(6), Degrees.of(-45)),
-    NEUTRAL_RIGHT(Meters.of(8), Meters.of(2), Degrees.of(45)),
-    NEUTRAL_LEFT_BACK(Meters.of(8), Meters.of(6), Degrees.of(-135)),
-    NEUTRAL_RIGHT_BACK(Meters.of(8), Meters.of(2), Degrees.of(135)),
-    NEAR_LEFT(Meters.of(1), Meters.of(6), Degrees.of(-45)),
-    NEAR_RIGHT(Meters.of(1), Meters.of(2), Degrees.of(45)),
-    FAR_LEFT(Meters.of(2), Meters.of(6), Degrees.of(-30)),
-    FAR_RIGHT(Meters.of(2), Meters.of(2), Degrees.of(30)),
-    CLIMB_LEFT(Meters.of(1), Meters.of(4.5), Degrees.of(-180)),
-    CLIMB_RIGHT(Meters.of(1), Meters.of(3.5), Degrees.of(-180));
+    NEUTRAL_LEFT(Meters.of(8), Meters.of(6), Degrees.of(135)),
+    NEUTRAL_RIGHT(Meters.of(8), Meters.of(2), Degrees.of(-135)),
+    NEUTRAL_LEFT_BACK(Meters.of(8), Meters.of(6), Degrees.of(135)),
+    NEUTRAL_RIGHT_BACK(Meters.of(8), Meters.of(2), Degrees.of(-135)),
+    NEAR_LEFT(Meters.of(2.25), Meters.of(5.5), Degrees.of(-45)),
+    NEAR_RIGHT(Meters.of(2.25), Meters.of(2.5), Degrees.of(45)),
+    FAR_LEFT(Meters.of(3.633), Meters.of(5.5), Degrees.of(-30)),
+    FAR_RIGHT(Meters.of(3.633), Meters.of(2.5), Degrees.of(30)),
+    CLIMB_LEFT(Meters.of(0.81), Meters.of(5.02), Degrees.of(-90)),
+    CLIMB_RIGHT(Meters.of(1.33), Meters.of(2.6), Degrees.of(90));
 
     private final Pose2d pose_;
 
