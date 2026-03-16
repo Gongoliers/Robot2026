640a711b8a28c23c536d2cb7bae6de61ca987ac7
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2026-03-16 12:21:00 -0400

    superstructure commands

--- src/main/java/frc/robot/superstructure/Superstructure.java
+++ src/main/java/frc/robot/superstructure/Superstructure.java
@@ -1,9 +1,14 @@
 package frc.robot.superstructure;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.Subsystem;
 import frc.robot.intake.Intake;
+import frc.robot.intake.IntakeState;
 import frc.robot.kicker.Kicker;
+import frc.robot.kicker.KickerState;
 import frc.robot.spindexer.Spindexer;
+import frc.robot.spindexer.SpindexerState;
 import frc.robot.turret.Turret;
 
 /**
@@ -61,6 +66,69 @@ public class Superstructure extends Subsystem {
 
   @Override
   public void periodic() {
-    
+
+  }
+
+  public Command init() {
+    return Commands.parallel(
+      turret.stow(),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(
+      intake.goToState(IntakeState.INIT)
+    ).finallyDo(() -> state = SuperstructureState.INIT);
+  }
+
+  public Command stow() {
+    return Commands.parallel(
+      turret.stow(),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(
+      intake.goToState(IntakeState.STOW)
+    ).finallyDo(() -> state = SuperstructureState.STOW);
+  }
+
+  public Command idle() {
+    return Commands.parallel(
+      intake.goToState(IntakeState.OUT),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(
+      turret.stow()
+    ).finallyDo(() -> state = SuperstructureState.IDLE);
+  }
+
+  public Command faceHub() {
+    return Commands.parallel(
+      intake.goToState(IntakeState.OUT),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(
+      turret.faceHub()
+    ).finallyDo(() -> state = SuperstructureState.FACE_HUB);
+  }
+
+  public Command intake() {
+    return Commands.parallel(
+      intake.goToState(IntakeState.OUT),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(Commands.parallel(
+      intake.goToState(IntakeState.INTAKE),
+      turret.faceHub()
+    )).finallyDo(() -> state = SuperstructureState.INTAKE);
+  }
+
+  public Command score() {
+    return Commands.parallel(
+      intake.goToState(IntakeState.OUT),
+      kicker.goToState(KickerState.STOP),
+      spindexer.goToState(SpindexerState.STOP)
+    ).andThen(Commands.parallel(
+      intake.goToState(IntakeState.AGITATE),
+      turret.targetHub()
+    ))
+    .finallyDo(() -> state = SuperstructureState.SCORE);
   }
 }
