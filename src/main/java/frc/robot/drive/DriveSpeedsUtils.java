f7c6d387f594fef6735b283ab0a053846479d3f8
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2026-03-23 17:03:46 -0400

    add ChassisSpeeds acceleration limiting

--- src/main/java/frc/robot/drive/DriveSpeedsUtils.java
+++ src/main/java/frc/robot/drive/DriveSpeedsUtils.java
@@ -1,13 +1,23 @@
 package frc.robot.drive;
 
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.RobotConstants;
 
 /** Class with static methods to help with getting chassis speeds to drive the drive subsystem */
 public class DriveSpeedsUtils {
+
+  /** Drive subsystem reference */
+  private static Drive drive = Drive.getInstance();
 
   /**
    * Gets field relative chassis speeds from controller input with some max linear and angular velocity
@@ -15,7 +25,7 @@ public class DriveSpeedsUtils {
    * @param controller Controller to grab inputs from
    * @param maxLinearVelocity Maximum desired linear velocity
    * @param maxAngularVelocity Maximum desired angular velocity
-   * @param deadzone Controller deadzone, by default uses deadzone of 0.1
+   * @param deadzone Controller deadzone (uses deadzone of 0.1 by default)
    * @return Field relative chassis speeds constructed from controller input with some max linear and angular velocity
    */
   public static ChassisSpeeds fromController(CommandXboxController controller, LinearVelocity maxLinearVelocity, AngularVelocity maxAngularVelocity, double deadzone) {
@@ -45,5 +55,43 @@ public class DriveSpeedsUtils {
    */
   public static ChassisSpeeds fromController(CommandXboxController controller, LinearVelocity maxLinearVelocity, AngularVelocity maxAngularVelocity) {
     return fromController(controller, maxLinearVelocity, maxAngularVelocity, 0.1);
+  }
+
+  /**
+   * Returns chassis speeds with limited linear and angular acceleration relative to the robot's current speeds
+   * 
+   * @param speeds Chassis speeds to be limited
+   * @param maxLinearAcceleration Maximum desired linear acceleration
+   * @param maxAngularAcceleration Maximum desired angular acceleration
+   * @param dt Time passed between calls (uses RobotConstants.PERIODIC_DURATION by default)
+   * @return chassis speeds iwth limited linear and angular acceleration relative to the robot's current speeds
+   */
+  public static ChassisSpeeds limitAcceleration(ChassisSpeeds speeds, LinearAcceleration maxLinearAcceleration, AngularAcceleration maxAngularAcceleration, Time dt) {
+    ChassisSpeeds botSpeeds = drive.getState().Speeds;
+
+    Translation2d requestedVelocity = new Translation2d(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond);
+    Translation2d botVelocity = new Translation2d(botSpeeds.vxMetersPerSecond, botSpeeds.vyMetersPerSecond);
+    Translation2d requestedAcceleration = requestedVelocity.minus(botVelocity).div(dt.in(Seconds));
+    if (requestedAcceleration.getNorm() > maxLinearAcceleration.in(MetersPerSecondPerSecond)) {
+      requestedAcceleration = requestedAcceleration.div(requestedAcceleration.getNorm()).times(maxLinearAcceleration.in(MetersPerSecondPerSecond));
+    }
+    Translation2d accelerationLimitedVelocity = botVelocity.plus(requestedAcceleration.times(dt.in(Seconds)));
+
+    double requestedAngularAcceleration = (speeds.omegaRadiansPerSecond - botSpeeds.omegaRadiansPerSecond) / dt.in(Seconds);
+    double accelerationLimitedAngularVelocity = botSpeeds.omegaRadiansPerSecond + (MathUtil.clamp(requestedAngularAcceleration, -maxAngularAcceleration.in(RadiansPerSecondPerSecond), maxAngularAcceleration.in(RadiansPerSecondPerSecond)) * dt.in(Seconds));
+
+    return new ChassisSpeeds(accelerationLimitedVelocity.getX(), accelerationLimitedVelocity.getY(), accelerationLimitedAngularVelocity);
+  }
+
+  /**
+   * Returns chassis speeds with limited linear and angular acceleration relative to the robot's current speeds
+   * 
+   * @param speeds Chassis speeds to be limited
+   * @param maxLinearAcceleration Maximum desired linear acceleration
+   * @param maxAngularAcceleration Maximum desired angular acceleration
+   * @return chassis speeds iwth limited linear and angular acceleration relative to the robot's current speeds
+   */
+  public static ChassisSpeeds limitAcceleration(ChassisSpeeds speeds, LinearAcceleration maxLinearAcceleration, AngularAcceleration maxAngularAcceleration) {
+    return limitAcceleration(speeds, maxLinearAcceleration, maxAngularAcceleration, RobotConstants.PERIODIC_DURATION);
   }
 }
